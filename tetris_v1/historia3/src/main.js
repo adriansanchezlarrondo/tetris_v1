1da7d9f51111bcbab4e08bedacd5c4df058e5103
adriansanchezlarrondo <123000464+adriansanchezlarrondo@users.noreply.github.com>
2024-02-15 19:10:13 +0100

    Delete tetris_v1 directory

--- tetris_v1/historia3/src/main.js
+++
@@ -1,7 +0,0 @@
-import { header } from './componentes/header'
-import { home } from './vistas/home'
-
-document.querySelector('header').innerHTML = header.template
-header.script()
-document.querySelector('main').innerHTML = home.template
-home.script()
