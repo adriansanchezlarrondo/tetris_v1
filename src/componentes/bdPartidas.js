f7f8bf77541329c4712d8e6899643395c8e52302
adriansanchezlarrondo <123000464+adriansanchezlarrondo@users.noreply.github.com>
2024-02-16 16:45:11 +0100

    Add files via upload

---
+++ src/componentes/bdPartidas.js
@@ -0,0 +1,20 @@
+export const partidas = [
+  {
+    avatar: 'https://www.svgrepo.com/show/384669/account-avatar-profile-user-13.svg',
+    nick: 'MANUEL',
+    puntos: 520,
+    fecha: '27 SEPTIEMBRE 2023'
+  },
+  {
+    avatar: 'https://www.svgrepo.com/show/384669/account-avatar-profile-user-13.svg',
+    nick: 'JOSEMA',
+    puntos: 150,
+    fecha: '2 MARZO 2023'
+  },
+  {
+    avatar: 'https://www.svgrepo.com/show/384671/account-avatar-profile-user-14.svg',
+    nick: 'MARIA',
+    puntos: 650,
+    fecha: '5 MAYO 2023'
+  }
+]
---
+++ src/componentes/header.js
@@ -0,0 +1,34 @@
+import { home } from '../vistas/home'
+import { juego } from '../vistas/juego'
+import { ranking } from '../vistas/ranking'
+
+export const header = {
+  template: // html
+    `
+    <nav class="navbar navbar-light bg-dark">
+      <div class="container-fluid">
+        <div class="mx-auto">
+          <button id="vistaHome" class="btn btn-success mx-2 fs-4">HOME</button>
+          <button id="vistaRanking" class="btn btn-success mx-2 fs-4">RANKING</button>
+          <button id="vistaJuego" class="btn btn-success mx-2 fs-4">JUEGO</button>
+        </div>
+      </div>
+    </nav>
+    `,
+  script: () => {
+    document.querySelector('#vistaHome').addEventListener('click', () => {
+      document.querySelector('main').innerHTML = home.template
+      home.script()
+    })
+
+    document.querySelector('#vistaRanking').addEventListener('click', () => {
+      document.querySelector('main').innerHTML = ranking.template
+      ranking.script()
+    })
+
+    document.querySelector('#vistaJuego').addEventListener('click', () => {
+      document.querySelector('main').innerHTML = juego.template
+      juego.script()
+    })
+  }
+}
